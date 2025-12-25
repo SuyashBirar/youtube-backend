97986eda15efa425a7fe81e18a6a28c522dffeda
Suyash <birarsuyash2629@gmail.com>
2025-12-25 16:02:22 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ dotenv.config({
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running on port: ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("Databse connection failed: ", error)
+    })
